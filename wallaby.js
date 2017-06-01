57a133331c63cdf0413daa83332606551ad4b510
Artem Govorov <artem.govorov@gmail.com>
2017-06-01 13:28:59 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -4,25 +4,16 @@ module.exports = function (wallaby) {
       'package.json',
       'src/**/*.ts',
       'src/**/*.js',
+      '!src/**/*.test.ts',
+      '!src/**/*.test.js'
     ],
     tests: [
       'src/**/*.test.ts',
       'src/**/*.test.js'
     ],
-    compilers: {
-        '**/*.ts': wallaby.compilers.typeScript({
-        // TypeScript compiler specific options
-        // https://github.com/Microsoft/TypeScript/wiki/Compiler-Options
-        // (no need to duplicate tsconfig.json, if you have it, it'll be automatically used)
-      })
-    },
+
     testFramework: 'jest',
     env: { type: 'node', runner: 'node' },
-    debug: true,
-    setup: function (wallaby) {
-      var jestConfig = require('./package.json').jest;
-      jestConfig.globals = { "__DEV__": true };
-      wallaby.testFramework.configure(jestConfig);
-    }
+    debug: true
   };
 };
